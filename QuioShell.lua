96efe53725df81e16423b998b84c6022e6eeeeb4
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-04 17:09:40 +0900

    Update QuioShell.lua

--- QuioShell.lua
+++ QuioShell.lua
@@ -1,5 +1,11 @@
 while true do
-    write(shell.dir() .. ":~$ ")
-    cmd = read()
-    shell.run(cmd)
+    if shell.dir() == nil then
+        write("/:~$ ")
+        cmd = read()
+        shell.run(cmd)
+    else
+        write(shell.dir() .. ":~$ ")
+        cmd = read()
+        shell.run(cmd)
+    end
 end
