72fda63d7a6ddd7a75dea23f50b4e9769738f895
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-04 10:27:34 +0900

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,4 +1,4 @@
-local buildID = 25
+local buildID = 157
 term.clear()
 shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/buildID.txt")
 local file = fs.open("buildID.txt", "r")
@@ -6,10 +6,12 @@ local data = file.readAll()
 
 if tonumber(data) > buildID then
   print("Updating... Please Wait...")
+  shell.run("rm /Quiologin.lua")
   shell.run("rm /QuioShell.lua")
   shell.run("rm /update.lua")
-  shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/QuioShell.lua")
+  shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/Quiologin.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/update.lua")
+  shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/QuioShell.lua")
   shell.run("rm buildID.txt")
   shell.run("reboot")
 else
