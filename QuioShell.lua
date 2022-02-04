5ab4250a9d22196b3f2d624858ee84a22ad0b07a
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-04 10:07:32 +0900

    Update QuioShell.lua

--- QuioShell.lua
+++ QuioShell.lua
@@ -1,19 +1,3 @@
-local buildID = 7
-term.clear()
-shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/buildID.txt")
-local file = fs.open("buildID.txt", "r")
-local data = file.readAll()
-
-if tonumber(data) > buildID then
-  print("Updating... Please Wait...")
-  shell.run("rm /QuioShell.lua")
-  shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/QuioShell.lua")
-  shell.run("rm buildID.txt")
-  shell.run("reboot")
-else
-    shell.run("rm buildID.txt")
-end
-
 print("User?")
 user = read("*")
 term.clear()
@@ -29,7 +13,5 @@ end
 while true do
     write(user .. ":~" .. shell.dir() .. "$ ")
     cmd = read()
-    shell.run("cd /home/." .. user)
     shell.run(cmd)
-    shell.run("cd /")
 end
