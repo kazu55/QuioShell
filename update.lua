7eadf2663b8df0bee7d2190cb63115365d133ae0
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-06 18:18:13 +0900

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,4 +1,4 @@
-local buildID = 2670
+local buildID = 3000
 term.clear()
 shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/buildID.txt")
 local file = fs.open("buildID.txt", "r")
