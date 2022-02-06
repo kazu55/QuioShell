671b4beab7f3711fcb65e31c4c6656e6d02e6e1b
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-06 16:16:28 +0900

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -11,6 +11,13 @@ os.fadeout = function(time)
   sleep(time)
 end
 
+local function centerText(text)
+  local x,y = term.getSize()
+  local x2, y2 = term.getCursorPos()
+  term.setCursorPos(math.ceil((x / 2) - (text:len() / 2)), y2)
+  write(text)
+end
+
 local reboot = os.reboot
 
 os.reboot = function()
@@ -22,13 +29,6 @@ os.reboot = function()
   while true do
     coroutine.yield()
   end
-end
-
-local function centerText(text)
-  local x,y = term.getSize()
-  local x2, y2 = term.getCursorPos()
-  term.setCursorPos(math.ceil((x / 2) - (text:len() / 2)), y2)
-  write(text)
 end
 
 local shutdown = os.shutdown
