99284069b8218fcfa829f2e750ba7a8c394b4983
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-13 13:13:16 +0900

    Update Quiologin.lua

--- Quiologin.lua
+++ Quiologin.lua
@@ -9,7 +9,7 @@ if not fs.exists("/home/." .. user .. ".pass.xyz") then
     passwrite = fs.open("/home/." .. user .. ".pass.xyz", "w")
     passwrite.write(pass)
     passwrite.close()
-    shell.run("hometab.lua")
+    shell.run("QuioShell.lua")
 end
 
 passread = fs.open("/home/." .. user .. ".pass.xyz", "r")
@@ -18,7 +18,7 @@ if pass == passrA then
     term.clear()
     term.setCursorPos(1,1)
     print("Login successful.")
-    shell.run("hometab.lua")
+    shell.run("QuioShell.lua")
 else
     print("Login Failed.")
     return
@@ -28,5 +28,5 @@ term.clear()
 term.setCursorPos(1,1)
 if fs.exists("/home/." .. user) then
     print("Login was successful.")
-    shell.run("/hometab.lua")
+    shell.run("/QuioShell.lua")
 end
