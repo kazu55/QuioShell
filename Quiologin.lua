1da0df01ecdb09654f7959734ee0aefe744164be
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-14 18:55:56 +0900

    Add files via upload

--- Quiologin.lua
+++ Quiologin.lua
@@ -3,6 +3,7 @@ term.setTextColor(colors.black)
 term.clear()
 term.setCursorPos(1,1)
 write("User: ")
+
 user = read()
 write("Password: ")
 pass = read("\7")
@@ -11,12 +12,16 @@ term.setTextColor(colors.white)
 term.clear()
 term.setCursorPos(1,1)
 
+if user == "" then
+    print("Wrong.")
+    return
+end
 if not fs.exists("/home/." .. user .. ".pass.xyz") then
     shell.run("mkdir /home/." .. user)
     passwrite = fs.open("/home/." .. user .. ".pass.xyz", "w")
     passwrite.write(pass)
     passwrite.close()
-    shell.run("QuioShell.lua")
+    shell.run("iconlauncher.lua")
 end
 
 passread = fs.open("/home/." .. user .. ".pass.xyz", "r")
@@ -24,10 +29,10 @@ passrA = passread.readAll()
 if pass == passrA then
     term.clear()
     term.setCursorPos(1,1)
-    print("Login successful.")
-    shell.run("QuioShell.lua")
+    shell.run("iconlauncher.lua")
 else
-    print("Login Failed.")
+    print("Wrong.")
+    sleep(1)
     return
 end
 
@@ -35,5 +40,5 @@ term.clear()
 term.setCursorPos(1,1)
 if fs.exists("/home/." .. user) then
     print("Login was successful.")
-    shell.run("/QuioShell.lua")
+    shell.run("/iconlauncher.lua")
 end
