ce09025fb8636702a56aad6fc1b14573f29ef82a
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-15 13:14:31 +0900

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,4 +1,4 @@
-local buildID = 4100
+local buildID = 4101
 term.clear()
 shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/buildID.txt")
 local file = fs.open("buildID.txt", "r")
@@ -12,12 +12,14 @@ if tonumber(data) > buildID then
   shell.run("rm /startup.lua")
   shell.run("rm /hometab.lua")
   shell.run("rm /iconlauncher.lua")
+  shell.run("rm /mode.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/Quiologin.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/update.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/QuioShell.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/startup.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/iconlauncher.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/adduser.lua")
+  shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/mode.lua")
   shell.run("rm buildID.txt")
   shell.run("reboot")
 else
