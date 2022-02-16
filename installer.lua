281c5436a8bfa43357261a77a4b3d0011868f442
たまさん <58621773+kazu55@users.noreply.github.com>
2022-02-16 17:16:26 +0900

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -5,7 +5,7 @@
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/startup.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/iconlauncher.lua /System/Programs/iconlauncher.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/adduser.lua /System/Programs/adduser.lua")
-  shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/APIs/QuioAPI.lua")
+  shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/APIs/QuioAPI.lua /APIs/QuioAPI.lua")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/APIs/QuioWindow.pic /APIs/QuioWindow.pic")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/APIs/err.pic /APIs/err.pic")
   shell.run("wget https://raw.githubusercontent.com/kazu55/QuioShell/master/APIs/info.pic /APIs/info.pic")
